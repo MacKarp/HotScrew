07ee3f2ff138c37b79afad178c577bb778fe5c9d
Karpfly <karpfly@gmail.com>
2021-12-05 12:42:50 +0100

    Added Navigation Bar

--- HotScrew/src/screens/Home_Screen.js
+++ HotScrew/src/screens/Home_Screen.js
@@ -1,6 +1,9 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import Card from '../components/Profile_Card';
 import users from '../../assets/data/users';
@@ -24,6 +27,13 @@ const HomeScreen = () => {
         onSwipeLeft={onSwipeLeft}
         onSwipeRight={onSwipeRight}
       />
+      <View style={styles.bottomNavigationBar}>
+        <FontAwesome name="undo" size={40} color="#eecf00" />
+        <Entypo name="cross" size={50} color="#c30000" />
+        <Entypo name="star" size={40} color="#3caecb" />
+        <Entypo name="heart" size={40} color="#6dc900" />
+        <Ionicons name="flash" size={40} color="#a200ff" />
+      </View>
     </View>
   );
 };
@@ -33,7 +43,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     flex: 1,
-    width:'100%'
+    width: '100%',
+  },
+  bottomNavigationBar: {
+flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#202020',
+    alignItems: 'center',
   },
 });
 
